3147d00f0d0abf65d008e0f7b987865a2d6d1cad
t18cs048 <t18cs048@gmail.com>
2024-01-21 23:00:17 +0900

    change[picslider]

--- js/mainstyle.js
+++ js/mainstyle.js
@@ -43,29 +43,14 @@ return false;//リンク自体の無効化
 });
 
 /*==画像スライダー==*/
-$('.slider').slick({
-	arrows: false,//左右の矢印はなし
-	autoplay: true,//自動的に動き出すか。初期値はfalse。
-	autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
-	speed: 6900,//スライドのスピード。初期値は300。
-	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-	pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
-	pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
-	cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
-	slidesToShow: 4,//スライドを画面に4枚見せる
-	slidesToScroll: 1,//1回のスライドで動かす要素数
-	responsive: [
-		{
-		breakpoint: 769,//モニターの横幅が769px以下の見せ方
-		settings: {
-			slidesToShow: 2,//スライドを画面に2枚見せる
-		}
-	},
-	{
-		breakpoint: 426,//モニターの横幅が426px以下の見せ方
-		settings: {
-			slidesToShow: 1.5,//スライドを画面に1.5枚見せる
-		}
-	}
-]
-});
+$(function(){
+	$(".slideshow-fade li").css({"position":"relative","overflow":"hidden"});
+	$(".slideshow-fade li").hide().css({"position":"absolute","top":0,"left":0});
+	$(".slideshow-fade li:first").addClass("fade").show();
+	setInterval(function(){
+	  var $active = $(".slideshow-fade li.fade");
+	  var $next = $active.next("li").length?$active.next("li"):$(".slideshow-fade li:first");
+	  $active.fadeOut(1000).removeClass("fade");
+	  $next.fadeIn(1000).addClass("fade");
+	},2000);
+  });
