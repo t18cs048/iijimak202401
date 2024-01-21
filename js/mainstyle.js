8b4eba9fe17b734533a90aa4807798427b83d770
t18cs048 <t18cs048@gmail.com>
2024-01-21 21:20:14 +0900

    add[pageup]

--- js/mainstyle.js
+++ js/mainstyle.js
@@ -1,20 +1,43 @@
-//スクロールすると上部に固定させるための設定を関数でまとめる
-function FixedAnime() {
-	var headerH = $('#header').outerHeight(true);
-	var scroll = $(window).scrollTop();
-	if (scroll >= headerH){//headerの高さ以上になったら
-			$('#header').addClass('fixed');//fixedというクラス名を付与
-		}else{//それ以外は
-			$('#header').removeClass('fixed');//fixedというクラス名を除去
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+
+	var scroll = $(window).scrollTop(); //スクロール値を取得
+	if (scroll >= 200){//200pxスクロールしたら
+		$('#page-top').removeClass('DownMove');		// DownMoveというクラス名を除去して
+		$('#page-top').addClass('UpMove');			// UpMoveというクラス名を追加して出現
+	}else{//それ以外は
+		if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名が既に付与されていたら
+			$('#page-top').removeClass('UpMove');	//  UpMoveというクラス名を除去し
+			$('#page-top').addClass('DownMove');	// DownMoveというクラス名を追加して非表示
 		}
+	}
+	
+	var wH = window.innerHeight; //画面の高さを取得
+	var footerPos =  $('#footer').offset().top; //footerの位置を取得
+	if(scroll+wH >= (footerPos+10)) {
+		var pos = (scroll+wH) - footerPos+10 //スクロールの値＋画面の高さからfooterの位置＋10pxを引いた場所を取得し
+		$('#page-top').css('bottom',pos);	//#page-topに上記の値をCSSのbottomに直接指定してフッター手前で止まるようにする
+	}else{//それ以外は
+		if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名がついていたら
+			$('#page-top').css('bottom','10px');// 下から10pxの位置にページリンクを指定
+		}
+	}
 }
 
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
-	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
 });
 
 // ページが読み込まれたらすぐに動かしたい場合の記述
 $(window).on('load', function () {
-	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+$('body,html').animate({
+	scrollTop: 0//ページトップまでスクロール
+}, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+return false;//リンク自体の無効化
 });
